eb1beaa0007a8b81f46e2d48d29c88ef7ca1c0d6
weriKK <peter@werik.com>
2018-11-02 04:33:04 +0100

    Build script fix

--- buildme.sh
+++ buildme.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-npm install webpack webpack-cli babel-core babel-plugin-transform-react-jsx babel-preset-env --save-dev
+npm install npx webpack webpack-cli babel-core babel-plugin-transform-react-jsx babel-preset-env --save-dev
 npx babel --plugins transform-react-jsx src/dashboard.js --out-file src/index.js
 npx webpack
